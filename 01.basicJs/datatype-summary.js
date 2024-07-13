ad1c51cde5db6da62fd9de74ca552d5c72b6d38b
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-13 19:26:50 +0000

    datatype-summary

---
+++ 01.basicJs/datatype-summary.js
@@ -0,0 +1,28 @@
+//Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, Bigint
+
+const score =  100
+const scoreValue = 100.3
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 8787677
+
+//--refrence(Non primitive)
+
+// Array, Object, Funtions
+
+const city = [bangluru, Indor, dehradun]
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
