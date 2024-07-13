972f7d2818ef102f2e84da41989e164d87a2badc
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-13 15:14:21 +0000

    conversionOperation

---
+++ 01.basicJs/03.conversionOperation.js
@@ -0,0 +1,19 @@
+let username = "komal"
+
+console.log(typeof username);
+
+let valueInNumber = Number(username)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33hf" => NaN
+//true => 1; false => 0
+
+let isLogginId = 1
+
+let booleanIsloggind = Boolean(isLogginId)
+console.log(booleanIsloggind);
+//"" => false
+// "komal" =. true
+
