c92c41e2ee9a59e87610db381cddffc03a99cea0
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-13 20:19:04 +0000

    String

---
+++ 01.basicJs/05.String.js
@@ -0,0 +1,19 @@
+const name = "komal"
+const repoCount = 50
+
+//console.log (name + repoCount + "Value");
+
+console.log(`Hello my name is ${name} an my repo count is ${repoCount}`);
+
+const myName = new String ( "komalsk")
+
+console.log(myName.length)
+console.log(myName.toUpperCase());
+console.log(myName.charAt(2));
+console.log(myName.indexOf('l'));
+
+const newString = myName.substring(0,4)
+console.log(newString);
+
+const anotherString = myName.slice(-7,4)
+console.log(anotherString);
