e2853920bd5bbb221ba437dd899c2c72658c9a77
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-13 14:36:12 +0000

    dataType

---
+++ 01.basicJs/02.dataType.js
@@ -0,0 +1,16 @@
+let name = "komal"
+let age = 19
+let isLogginId = true 
+
+
+//------type
+//number
+//bigint
+//string
+//boolean => flase/true
+// null => standalone value
+//undifinde 
+//symbol => unique
+
+
+console.log(typeof "komal");
