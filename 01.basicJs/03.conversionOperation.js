4260d273682a45d8162bac276c47501ec74cc154
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-13 19:00:52 +0000

    conversionOperation

--- 01.basicJs/03.conversionOperation.js
+++ 01.basicJs/03.conversionOperation.js
@@ -1,19 +1,34 @@
-let username = "komal"
+// let username = "komal"
 
-console.log(typeof username);
+// console.log(typeof username);
 
-let valueInNumber = Number(username)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber);
+// let valueInNumber = Number(username)
+// // console.log(typeof valueInNumber)
+// // console.log(valueInNumber);
 
-//"33" => 33
-//"33hf" => NaN
-//true => 1; false => 0
+// //"33" => 33
+// //"33hf" => NaN
+// //true => 1; false => 0
 
-let isLogginId = 1
+// let isLogginId = 1
 
-let booleanIsloggind = Boolean(isLogginId)
-console.log(booleanIsloggind);
+// let booleanIsloggind = Boolean(isLogginId)
+// console.log(booleanIsloggind);
 //"" => false
 // "komal" =. true
 
+//*********************Operation**********************
+
+let number = 3
+let negValue = -number
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = "komal"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
