c54247b4ef16da415daf3bf1eb16ed3b72e0c033
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-13 14:35:33 +0000

    variable

---
+++ 01.basicJs/01.variable.js
@@ -0,0 +1,18 @@
+const userName = "Komlal"
+let accountEmail = "komal12@gm.com"
+var accountPassword = "123@"
+cityName = "jaipur"
+let accountStatus;
+
+// console.log(username); // not allowed
+
+accountEmail = "kks1234@gm.com"
+accountPassword = "456@"
+cityName = "dehradun"
+
+/*
+prefer not to  use var
+beacuse of issue in block scope and functional scope
+*/
+
+console.table([ userName, accountEmail, accountPassword , cityName ])
