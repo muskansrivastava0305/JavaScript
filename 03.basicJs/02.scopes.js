238bb3fccf1db996f7f227d34d63b68a3d9754da
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-14 16:27:53 +0000

    Scopes

--- 03.basicJs/02.scopes.js
+++ 03.basicJs/02.scopes.js
@@ -1,14 +1,60 @@
-//var c = 300
-let a = 100
+// //var c = 300
+// let a = 100
+
+
+// if (true){
+//     let a = 10
+//     const b = 20
+//     var c = 30
+//     console.log("inner" , a);
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//*******************************************
+
+function one(){
+    const username = "komal"
+
+    function two(){
+        const website = "linkdin"
+        //console.log(username);
+    }
+    //console.log(website)
+
+    two()
+}
+
+one()
 
 
 if (true){
-    let a = 10
-    const b = 20
-    var c = 30
-    console.log("inner" , a);
+    const username = "komal"
+    if(username === "komal"){
+        const website = "linkdin"
+        //console.log(username + website)
+    }
+
+    //console.log(website);
 }
 
-console.log(a);
-console.log(b);
-console.log(c);
+//console.log(username);
+
+
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+
+console.log(addone(6))
+function addone(num){
+    return num + 1
+}
+
+
+//console.log(addTwo(7))
+const addTwo = function(num){ //expresssion
+    return num + 2
+}
+console.log(addTwo(7))
