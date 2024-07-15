0353dc8dbb5093d4c3df0c9f60742559ce2a9efb
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-15 09:19:38 +0000

    date & time

--- 01.basicJs/07.date.js
+++ 01.basicJs/07.date.js
@@ -1,3 +1,15 @@
 //Dates
 
-let myDate 
+let myDate = new Date()
+//  console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2001 , 4 , 3 , 6 ,7)
+console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
