233c4cb42761c9aa141b19e42cb1a9f12b5395bd
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-17 04:50:09 +0000

    arrow

--- 03.basicJs/03.arrow.js
+++ 03.basicJs/03.arrow.js
@@ -4,9 +4,40 @@ const user = {
 
     welcomeMessage: function(){
         console.log(`${this.username}, welcome to website`);
+        //console.log(this);
     }
 }
 
 user.welcomeMessage();
 user.username = "sam"
 user.welcomeMessage()
+
+console.log(this);
+
+
+// function hello() {
+//     let username = "komal"
+//     console.log(this.username);
+// }
+
+// //hello();
+
+// const hello = function () {
+//     let username ="komal"
+//     console.log(this.username);
+// }
+
+
+// const hello = function () {
+//     let username ="komal"
+//     console.log(this.username);
+// }
+
+const hello = () => {
+    let username = "komal"
+    console.log(this);
+}
+
+hello();
+
+
