63848da5c77d0fd59f1a30ade9d542de7273b616
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-18 12:43:27 +0000

    object

--- 02.BsicsJs/03.object.js
+++ 02.BsicsJs/03.object.js
@@ -1,11 +1,15 @@
 // object litrerals 
 
+const mySym = Symbol("key1")
+
 const user = {
     name: "komal",
     age: 19,
+    [mySym] : "myKey1",
     email : "hdgh52@gmail.com",
     isLoggedIn : false
 }
 
 console.log( user.email);
 console.log(user["age"]);
+console.log(user[mySym]);
