ccf56392872468394d903b0d159599908d0558fd
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-19 06:18:38 +0000

    LOop

--- 05.Loops/pracLoop.js
+++ 05.Loops/pracLoop.js
@@ -1,23 +1,45 @@
-/*======================for-loop==========================*/
-
-for (let i = 0; i < 5; i++) {
-    console.log(i);
-  }
-
-/*======================while-loop======================*/
-
-let i = 0;
-while (i < 5) {
-  console.log(i);
-  i++;
+let index = 0
+while (index <= 10) {
+  console.log(`value of index is ${index}`);
+  index = index + 2
 }
 
-/*====================do-while-loop====================*/
+let myArray = ['mango' , 'apple']
+let arr = 0
+while(arr<myArray.length){
+  console.log(`value is ${myArray[arr]}`);
+  arr = arr + 1
+}
 
-var j = 0;
+let score = 10
+
 do {
-  console.log(j);
-  j++;
-} while (j < 5);
+  console.log(`score is ${score}`);
+  score++;
+} while (score <= 10);
 
-/*==================================================*/
+
+
+// /*======================for-loop==========================*/
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+//   }
+
+// /*======================while-loop======================*/
+
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+
+// /*====================do-while-loop====================*/
+
+// var j = 0;
+// do {
+//   console.log(j);
+//   j++;
+// } while (j < 5);
+
+// /*==================================================*/
