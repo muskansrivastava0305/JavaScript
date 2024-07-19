28c3a696e2ae0bfef27c2ad661f0cd756386cbd5
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-19 05:39:53 +0000

    control flow

--- 04.control_flow/01.if.js
+++ 04.control_flow/01.if.js
@@ -1,3 +1,22 @@
+//short hand notetion
+const balance = 1000
+
+if (balance < 500) {
+  console.log("less than 500");
+} else if(balance < 750){
+  console.log("less than 750");
+}else if (balance < 900){
+  console.log("less than 750");
+}else{
+  console.log("less than 1200");
+}
+
+//falsy values
+//false, 0, -0, Bigint 0n, "" , null, undefined, NaN
+
+//truthy values
+//"0", 'false', " ", [], {}, function(){}
+
 /*================If-Statement=====================*/
 
 // var x = 10;
@@ -16,14 +35,14 @@
 
 /*=================Switch-Statement=================*/
 
-let day = "Sunday";
-switch (day) {
-  case "Monday":
-    console.log("Today is Monday");
-    break;
-  case "Tuesday":
-    console.log("Today is Tuesday");
-    break;
-  default:
-    console.log("Today is not Monday or Tuesday");
-}
+// let day = "Sunday";
+// switch (day) {
+//   case "Monday":
+//     console.log("Today is Monday");
+//     break;
+//   case "Tuesday":
+//     console.log("Today is Tuesday");
+//     break;
+//   default:
+//     console.log("Today is not Monday or Tuesday");
+// }
