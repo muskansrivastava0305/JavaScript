6d149fb30cd75b4f4b9bfc8946d28d1131337a12
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-20 07:11:13 +0000

    foreach

--- 05.Loops/06.foreach.js
+++ 05.Loops/06.foreach.js
@@ -1,27 +1,40 @@
-// const pname = ['kjjk' , 'jasdj' ,'asjk']
+// // const pname = ['kjjk' , 'jasdj' ,'asjk']
 
-// const value = pname.forEach((item) => {
-//     console.log(item);
-//     return item
+// // const value = pname.forEach((item) => {
+// //     console.log(item);
+// //     return item
+// // })
+
+// // console.log(value)
+
+
+// //--------------Filters-------------
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// // const newNums = myNums.filter ((num) => num > 3)
+// // console.log(newNums);
+
+// //foreach-------------------------
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num>4){
+//         newNums.push(num)
+//     }
 // })
 
-// console.log(value)
-
-
-//--------------Filters-------------
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-// const newNums = myNums.filter ((num) => num > 3)
 // console.log(newNums);
 
-//foreach-------------------------
+const books = [
+    {title: 'Book One' , genre: 'fiction' , publish: 1981, edition: 2004},
+    {title: 'Book One' , genre: 'history' , publish: 2003, edition: 2004},
+    {title: 'Book One' , genre: 'history' , publish: 2009, edition: 2004},
+    {title: 'Book One' , genre: 'fiction' , publish: 1981, edition: 2004}
+];
 
-const newNums = []
+let userBooks = books.filter((bk) => bk.genre === 'history')
 
-myNums.forEach( (num) => {
-    if (num>4){
-        newNums.push(num)
-    }
-})
-
-console.log(newNums);
+userBooks = books.filter((bk) => {
+    return bk.publish >= 2000 && bk.genre ==='history'})
+console.log(userBooks);
