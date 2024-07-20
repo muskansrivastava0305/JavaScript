5a4f9bb062f6b47999224227058f7c8064aa91e5
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-20 05:17:34 +0000

    Loop-Map

--- 05.Loops/three.js
+++ 05.Loops/three.js
@@ -1,3 +1,5 @@
+// highr order array loops
+
 // for of loop
 // ["" , "" , ""]
 ///[{}, {}, {}]
@@ -7,3 +9,33 @@ const arr = [1, 2, 3, 4, 5]
 for(const num of arr){
     console.log(num);
 }
+
+const greeting = 'Hello JavaScript'
+for(const greet of greeting){
+    console.log(greet);
+}
+
+//Maps 
+
+const map =new Map()
+map.set('USA' , "United states of America")//(key,value)
+map.set('IN' , "India")
+map.set('Fr', "France")
+map.set('IN' , "India")
+ 
+console.log(map);
+//Map in Loop------------------------------------
+
+for(const [key , value] of map){
+    console.log(key, ":=" ,value);
+}
+
+
+// const myobject = {
+//     'game1' : 'Nfs',
+//     'game2' : 'Ludo'
+// }
+
+// for (const [key , value] of Object){
+//      console.log(key , ":-" ,value);
+// }
