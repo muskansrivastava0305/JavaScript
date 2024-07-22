ea52619c56cac6ed055a85bc82c263adb599b38c
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-22 12:59:54 +0000

    Oop

--- 10. classes_Oop/Oop.js
+++ 10. classes_Oop/Oop.js
@@ -1,4 +1,18 @@
 //object litralls
 
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    greet() {
+      console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+  }
+  
+  const person = new Person('John', 30);
+  person.greet(); // Output: Hello, my name is John and I am 30 years old.
+
 //Constructor
 
