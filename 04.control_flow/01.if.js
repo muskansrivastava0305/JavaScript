f03efdbd0432a83ef0a82ae794167aee3414cdc2
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-22 08:04:28 +0000

    if

--- 04.control_flow/01.if.js
+++ 04.control_flow/01.if.js
@@ -37,30 +37,30 @@ teaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
 
 /*================If-Statement=====================*/
 
-// var x = 10;
-// if (x > 5) {
-//   console.log("x is greater than 5");
-// }
+var x = 10;
+if (x > 5) {
+  console.log("x is greater than 5");
+}
 
 /*================if-else-Statement=================*/
 
-// let x = 9;
-// if (x > 5) {
-//     console.log("x is greater then 5");
-// } else {
-//     console.log("x is less then 5")
-// }
+let x = 9;
+if (x > 5) {
+    console.log("x is greater then 5");
+} else {
+    console.log("x is less then 5")
+}
 
 /*=================Switch-Statement=================*/
 
-// let day = "Sunday";
-// switch (day) {
-//   case "Monday":
-//     console.log("Today is Monday");
-//     break;
-//   case "Tuesday":
-//     console.log("Today is Tuesday");
-//     break;
-//   default:
-//     console.log("Today is not Monday or Tuesday");
-// }
+let day = "Sunday";
+switch (day) {
+  case "Monday":
+    console.log("Today is Monday");
+    break;
+  case "Tuesday":
+    console.log("Today is Tuesday");
+    break;
+  default:
+    console.log("Today is not Monday or Tuesday");
+}
