05c27608470a47e23449783fa778e82e37d1b168
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-30 11:55:01 +0000

    Prototype

--- 11.Prototype/prototype.js
+++ 11.Prototype/prototype.js
@@ -16,7 +16,48 @@ let namesTitle = {
 }
 
 Object.prototype.muskan = function(){
-    console.log(`komal is present in alal objects`)
+    console.log(`komal is present in all objects`)
 }
 
-namesTitle.muskan();
+Array.prototype.heykomal = function(){
+    console.log(`komal say hello`)
+}
+//namesTitle.muskan();
+allNames.muskan()
+allNames.heykomal();
+
+//---------------------------------------------------------------------
+
+const user = {
+    name: "komal",
+    email: "jhsj@gmail.com"
+}
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TaSupport = {
+    makeAssignment: 'JS Assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__=user
+
+//modern syntax------------------------------
+
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUserName = " my name             "
+String.prototype.trueLength = function(){
+    console.log(`${(this)}`);
+    console.log(`${(this.name)}`);
+    console.log(`True lenght is : ${this.trim().length}`)
+}
+
+anotherUserName.trueLength();
+
