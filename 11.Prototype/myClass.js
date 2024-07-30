ef9285c7eeadc1f320b0a069013b7f64b4d2bf84
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-07-30 12:14:23 +0000

    myclass

---
+++ 11.Prototype/myClass.js
@@ -0,0 +1,17 @@
+// ES6
+
+class User {
+    constructor (username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    encryptPassword(){
+        return `${(this.password)}abc`
+    }
+}
+
+const chai = new User("chai", "chai@hmail.com", "123")
+
+console.log(chai.encryptPassword());
