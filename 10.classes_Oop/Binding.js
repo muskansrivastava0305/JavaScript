4fac431ea95eb6f08f358cc1a418000b53511d5d
Muskan Kumari <91183865+muskansrivastava0305@users.noreply.github.com>
2024-08-02 09:30:42 +0000

    Binding-Javascript

---
+++ 10.classes_Oop/Binding.js
@@ -0,0 +1,33 @@
+const obj = {
+    name: 'John',
+    sayHello: function() {
+      console.log(`Hello, my name is ${this.name}`);
+    }
+  };
+  
+  const boundSayHello = obj.sayHello.bind(obj);
+  boundSayHello(); 
+
+  //---Using an arrow function---------------------------------
+
+  const obj1 = {
+    name: 'John',
+    sayHello: () => {
+      console.log(`Hello, my name is ${this.name}`);
+    }
+  };
+  
+  obj.sayHello();
+
+   //---using a closure----------------------------------------
+   const obj2 = {
+    name: 'Komal',
+    sayHello: function() {
+      return function() {
+        console.log(`Hello, my name is ${this.name}`);
+      }.bind(this);
+    }
+  };
+  
+  const boundSayHello1 = obj.sayHello();
+  boundSayHello(); 
